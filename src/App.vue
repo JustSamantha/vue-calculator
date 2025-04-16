ddebe0c44bfa6b2ba84ce5909da0368fb7678535
Samantha Castillo <samanthacas@pm.me>
2025-04-16 14:06:57 -0700

    -Adds calculator logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <script setup lang="ts">
-  let calculator = "";
-  function addSymbol(event) {
-    console.log(event.target.innerText);
-    calculator = event.target.innerText;
+  import { ref } from 'vue';
+
+  const calculatorDisplay = ref('');
+  let currentSymbol = '';
+  let storedValue = '';
+  let clearInput = false;
+
+  function addInput(event:Event) {
+    if (event.target && event.target.innerText) {
+      const input = event.target.innerText;
+      if (isSymbol(input)) {
+        addSymbol(input);
+      } else if (input === '=') {
+        doMath();
+        currentSymbol = '';
+        storedValue = '';
+      } else if (input === 'C') {
+        currentSymbol = '';
+        storedValue = '';
+        clearInput = true;
+        calculatorDisplay.value = '';
+      } else {
+        addNumber(event.target.innerText);
+      }
+    }
+  }
+
+  function addNumber(number:string) {
+    if (clearInput) {
+      calculatorDisplay.value = '';
+      clearInput = false;
+    }
+    let tmpNumber:string = calculatorDisplay.value.toString();
+    tmpNumber += number;
+    console.log(tmpNumber);
+    calculatorDisplay.value = tmpNumber;
+  }
+
+  function doMath() {
+    calculatorDisplay.value = eval(storedValue + currentSymbol + calculatorDisplay.value);
+  }
+
+  function addSymbol(symbol:string) {
+    clearInput = true;
+    if (currentSymbol !== '') {
+      doMath();
+    }
+    currentSymbol = symbol;
+    storedValue = calculatorDisplay.value;
+  }
+
+  function isSymbol(char:string) {
+    const symbols:Array<string> = [ '+', '-', '/', '*'];
+    return !!symbols.find((symbol) => symbol === char);
   }
 </script>
 
 <template>
   <main>
-    <input type="text" v-bind:src="calculator" disabled="true" />
-    <button @click="addSymbol">0</button>
-    <button @click="addSymbol">1</button>
-    <button @click="addSymbol">2</button>
-    <button @click="addSymbol">3</button>
-    <button @click="addSymbol">4</button>
-    <button @click="addSymbol">5</button>
-    <button @click="addSymbol">6</button>
-    <button @click="addSymbol">7</button>
-    <button @click="addSymbol">8</button>
-    <button @click="addSymbol">9</button>
-    <button @click="addSymbol">+</button>
-    <button @click="addSymbol">-</button>
-    <button @click="addSymbol">/</button>
-    <button @click="addSymbol">*</button>
+    <input type="text" :value="calculatorDisplay" disabled="true" />
+    <button @click="addInput">1</button>
+    <button @click="addInput">2</button>
+    <button @click="addInput">3</button>
+    <button @click="addInput">4</button>
+    <button @click="addInput">5</button>
+    <button @click="addInput">6</button>
+    <button @click="addInput">7</button>
+    <button @click="addInput">8</button>
+    <button @click="addInput">9</button>
+    <button @click="addInput">0</button>
+    <button @click="addInput">.</button>
+    <button @click="addInput">+</button>
+    <button @click="addInput">-</button>
+    <button @click="addInput">/</button>
+    <button @click="addInput">*</button>
+    <button @click="addInput">=</button>
+    <button @click="addInput">C</button>
   </main>
 </template>
 
