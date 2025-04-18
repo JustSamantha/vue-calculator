7df5d5a814b5152472264cb37217c93e59cbf44b
Samantha Castillo <samanthacas@pm.me>
2025-04-18 12:01:33 -0700

    -Improves style

--- src/App.vue
+++ src/App.vue
@@ -58,33 +58,32 @@
 </script>
 
 <template>
-  <main>
-    <div class="w-[200px]">
-      <input class="border-1 w-full rounded text-right pr-[5px] pl-[5px]" type="text" :value="calculatorDisplay" disabled="true" />
+  <main class="flex h-screen justify-center bg-pink-100">
+    <div class="w-[200px] mt-10">
+      <input class="border-1 w-full rounded text-right pr-[5px] pl-[5px] h-[50px]" type="text" :value="calculatorDisplay" disabled="true" />
       <div class="mt-[5px] grid grid-cols-4 grid-rows-5">
-        <button class="bg-gray-100 hover:bg-green-500 border-1 rounded h-[30px]" @click="addInput">1</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">2</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">3</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">+</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">4</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">5</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">6</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">-</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">7</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">8</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">9</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">/</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">.</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">0</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">=</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">*</button>
-        <button class="border-1 rounded h-[30px]" @click="addInput">C</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">1</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">2</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">3</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">+</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">4</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">5</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">6</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">-</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">7</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">8</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">9</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">/</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">.</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">0</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">=</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">*</button>
+        <button class="hover:bg-gray-200 border-1 rounded h-[30px]" @click="addInput">C</button>
       </div>
     </div>
   </main>
 </template>
 
 <style scoped>
-@import "tailwindcss";
 
 </style>
